782ba55daec376fbf7c4bec7ac8ba989a93b4dc8
Nicholas Mills <nicholasrmills@icloud.com>
2019-02-20 11:14:38 +1100

    Replacing JSONServer with Mongoid, having fun and loving life (#2)
    * GET, GET SPECIFIC, DELETE, POST(broken) for mongoid
    
    * POST and PUT are killing the game
    
    * Making the best PUT method the world has seen
    
    * (almost) all tests passing! fun happy times
    
    * All tests passed. Life is g00000d

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require 'pry'
 require_relative 'environment'
 require 'json'
 
-
 before do
     response.headers["Access-Control-Allow-Methods"] = "POST", "OPTIONS", "PUT", "DELETE", "GET", "PATCH"
     response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
@@ -18,122 +17,84 @@ options "*" do
 end
 
 def rating_questions
-    JSON.parse(File.read('db.json'))['ratingQuestions']
-    # RatingQuestion.all.to_a.to_json
+    RatingQuestion.all.to_a
 end
 
-def replace_json(updated_questions_data)
-    new_json = { "ratingQuestions": updated_questions_data }
-    File.open("./db.json", "w"){ |f| f.write(new_json.to_json) }
+def send_response(response, status, body)
+    response.status = status
+    response.body = body.to_json
+    response
+end
+
+def serialize_question(question)
+    {
+      id: question.id.to_s,
+      title: question.title,
+      tag: question.tag
+    }
 end
 
 get '/ratingQuestions' do
-    # binding.pry
-    content_type :json
-    rating_questions.to_json
-    # RatingQuestion.all.to_json
+    new_questions = RatingQuestion.all.map{ |q| serialize_question(q) }
+    return send_response(response, 200, new_questions)
 end
 
 get '/ratingQuestions/:id' do
-    target_id = params["id"].to_i
-    question = rating_questions.find{ |q| q["id"] == target_id }
-    return response.status = 404 if question.nil?
+    target_id = params["id"]
+    question = RatingQuestion.find_by(_id: target_id)
+    return send_response(response, 404, {}) if question.nil?
 
-    response.status = 200 
-    response.body = question.to_json
-    response
+    return send_response(response, 200, serialize_question(question))
+end
+
+
+delete '/ratingQuestions/:id' do
+    target_id = params["id"]
+    # Find if the question exists
+    q_to_del = RatingQuestion.find(target_id)
+    return send_response(response, 404, {}) if q_to_del.nil?
+
+    # If it exists, kill it lol
+    q_to_del.destroy
+    return send_response(response, 204, {})
 end
 
 post '/ratingQuestions' do
-    # If they don't actually send us a body
-    if request.body.size.zero?
-        response.status = 400 
-        response.body = {}.to_json
-        return response
-    end
+    body = request.body.read 
+    return send_response(response, 400, nil) if body.size.zero?
 
-    json_params = JSON.parse(request.body.read) 
-
-    # If the title is empty, return an error 
-    if json_params["title"] == ''
-        response.status = 422
-        # TODO: investigate cleaner way of sending errors 
-        response.body = {"errors" => {"title" => ["cannot be blank"]}}.to_json
-        return response
-    end
-
-    new_question = {"title" => json_params["title"], "id" => rating_questions.any? ? rating_questions.last["id"]+1 : 1}    
-    new_questions = rating_questions << new_question
-    status 201
-
-    # Now lets replace the file with this new array 
-    replace_json(new_questions)
-    new_question.to_json
-end
-
-delete '/ratingQuestions/:id' do
-    target_id = params["id"].to_i
-    # Return a 404 if it's not in the array 
-
-    question = rating_questions.find{ |q| q["id"] == target_id }
-    return response.status = 404 if question.nil?
-
-    new_questions = rating_questions.reject { |q| q["id"] == target_id }
-
-    if new_questions
-        # Put the new array into JSON
-        replace_json(new_questions)
-        response.status = 204 
-        # response.body = {}.to_json
-    end
-    return response
+    json_params = JSON.parse(body) 
+    
+    new_question = RatingQuestion.new(title: json_params["title"], tag: json_params["tag"])
+    # binding.pry
+    if new_question.save
+        send_response(response, 201, serialize_question(new_question))
+    else
+        errors = { "errors" => new_question.errors.messages }
+        return send_response(response, 422, errors )
+    end    
 end
 
 put '/ratingQuestions/:id' do
-    # In case of empty body
-    if (request.body.nil?)
-        return response
-    end
-
-    target_id = params["id"].to_i
-    
-    updated_questions = rating_questions
-    question = updated_questions.find{ |q| q["id"] == target_id }
-   
-    if (question.nil? || target_id == 0)
-        response.status = 404
-        return response
-    end
-
+    # binding.pry
     json_params = JSON.parse(request.body.read) 
+    question = RatingQuestion.find_by(id: params["id"])
+    return send_response(response, 404, {}) if json_params.nil? || question.nil?
 
-    # If all is well and we have a question object, set that shit
-    question = {"id": target_id, "title": json_params["title"], tag: json_params["tag"]}.to_json
-    response.body = question
-    response.status = 200
-    response
+    # If it exists, lets edit it
+    question.update(title: json_params["title"], tag: json_params["tag"])
+    return send_response(response, 200, serialize_question(question))
 end
 
-patch '/ratingQuestions/:id' do 
-      # In case of empty body
-      if (request.body.nil?)
-        return response
-    end
-
-    target_id = params["id"].to_i
-    
-    updated_questions = rating_questions
-    question = updated_questions.find{ |q| q["id"] == target_id }
-   
-    if (question.nil? || target_id == 0)
-        response.status = 404
-        return response
-    end
-
+patch '/ratingQuestions/:id' do
     json_params = JSON.parse(request.body.read) 
-    question.merge!(json_params)
+    question = RatingQuestion.find_by(id: params["id"])
+    return send_response(response, 404, {}) if json_params.nil? || question.nil?
 
-    response.body = question
-    response.status = 200
-    response
+    # Don't replace unless we were given a title - I'm sure this could be written cleaner
+    title = json_params["title"].nil? ? question.title : json_params["title"]
+    tag = json_params["tag"].nil? ? question.tag : json_params["tag"]
+
+    question.update(title: title, tag: json_params["tag"])
+    return send_response(response, 200, serialize_question(question))
 end
